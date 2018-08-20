6367f534fd7f7a6d842795be9a6ee8fd854cf151
Andrea Rose <a.rose.siej@gmail.com>
2018-08-20 15:03:57 -0400

    Named Exports exercise

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+console.log('utils.js is running');
+
+export const square = (x) => x*x;
+export const add = (a,b) => a+b;
+
+//exports - default export - named exports
+// alt syntax - export { square, add }
+// must have variable declaration to export
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
-//entry - where does app kick off
-// ->
-//output - where to put this
-
 const path = require('path');
 
 // use to expose obj to another file
 module.exports = {
     // config details
     // where to start? - main file
+    // loaded and executed by default
     entry: './src/app.js',
     output: {
         // absolute path to where we want to output this file
