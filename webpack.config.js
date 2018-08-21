fed2864ce5f3ff396c7b40053eac3348932f4fac
Andrea Rose <a.rose.siej@gmail.com>
2018-08-21 16:32:09 -0400

    Installed babel-core and bable-loader for Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 // use to expose obj to another file
+// https://webpack.js.org/
 module.exports = {
     // config details
     // where to start? - main file
@@ -11,6 +12,19 @@ module.exports = {
         // path to current location
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+    module: {
+
+        // defines how we want to use our loader
+        // run babel whenever it sees a JS file except the ones in
+        // the node_modules
+        rules: [{
+            loader: 'babel-loader',
+            // what files we want to load this on - js extension
+            test: /\.js$/,
+            // don't run babel on these libraries
+            exclude: /node_modules/
+        }]
     }
 
 };
