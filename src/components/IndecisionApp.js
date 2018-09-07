933c016ed23d9a421908d2e22436f67bb2884f9f
Andrea Rose <a.rose.siej@gmail.com>
2018-09-07 16:19:57 -0400

    Created SCSS variables in _settings.scss and began styling app

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -158,24 +158,28 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header />
-                <Action
-                    hasOptions={this.state.options.length > 0}
-                    handlePick={this.handlePick}
-                />
-                <Options
-                    options={this.state.options}
-                    // allows access to this option via the option class
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption={this.handleDeleteOption}
-                />
-                <AddOption
-                    handleAddOption={this.handleAddOption}
-                />
+                <div className="container">
+                    <Action
+                        hasOptions={this.state.options.length > 0}
+                        handlePick={this.handlePick}
+                    />
+                    <Options
+                        options={this.state.options}
+                        // allows access to this option via the option class
+                        handleDeleteOptions={this.handleDeleteOptions}
+                        handleDeleteOption={this.handleDeleteOption}
+                    />
+                    <AddOption
+                        handleAddOption={this.handleAddOption}
+                    />
+                </div>
+                
 
-                <OptionModal 
+                <OptionModal
                     selectedOptionModal={this.state.selectedOptionModal}
                     handleClearSelectedOption={this.handleClearSelectedOption}
                 />
+                
             </div>
         );
     }
